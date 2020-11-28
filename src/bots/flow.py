f39d86fdf8867ff9ce48cb343ef178d134090509
sebhauri <shauri@netplus.ch>
2020-11-28 17:36:03 +0100

    Implemented until LOCATION state. Need to fix SLEEP state

--- src/bots/flow.py
+++ src/bots/flow.py
@@ -14,78 +14,80 @@ from telegram import (
 
 from enum import Enum
 
+import logging
+import datetime
+
 class State(Enum):
     SLEEP = 0
     START = 1
     DATE = 2
+    DATE_BIS = 3
+    LOCATION = 4
 
 TOKEN = '1432985981:AAHxLzTlnqVH8uo20PPuhDFSqbWqp6hBlJw'
 
-def start(update: Update, context: CallbackContext) -> None:
-    context.user_data['state'] = State.START
-
+def start(update: Update, context: CallbackContext) -> State:
     button = [["Yes !", "No :("]]
     update.effective_message.reply_text(
         "Welcome ! Would you like to creat an event ?", reply_markup=ReplyKeyboardMarkup(button, one_time_keyboard=True)
     )
+    return State.START
 
-def resp(update: Update, context: CallbackContext):
-    switcher = {
-        State.SLEEP : sleep_response,
-        State.START : start_response,
-        State.DATE : date_response,
-    }
-
-    state = context.user_data['state']
-    switcher.get(state, error_response)(update, context)
-    
-
-def error_response(update: Update, context: CallbackContext) -> None:
+def error_response(update: Update, context: CallbackContext) -> State:
     update.message.reply_text("Something went wrong, please start over with /start")
 
-def sleep_response(update: Update, context: CallbackContext) -> None:
+def sleep_response(update: Update, context: CallbackContext) -> State:
     update.message.reply_text("Use /start to start planning an event")
+    return State.START
 
-def start_response(update: Update, context: CallbackContext) -> None:
-    if 'No :(' == update.effective_message.text:
-        context.user_data['state'] = State.SLEEP
+def start_response(update: Update, context: CallbackContext) -> State:
+    if  update.effective_message.text == 'No :(':
         update.message.reply_text("Sorry to see you go :(")
-    elif 'Yes !' == update.effective_message.text:
-        context.user_data['state'] = State.DATE
+        return State.SLEEP
+    elif update.effective_message.text == 'Yes !':
         update.message.reply_text("Propose a date (format : dd/mm)")
+        return State.DATE
 
-def date_response(update: Update, context: CallbackContext) -> None:
-    context.user_data['state'] = State.SLEEP
-    update.message.reply_text(f"Your date is : {update.effective_message.text}")
+def date_response(update: Update, context: CallbackContext) -> State:
+    update.message.reply_text("Type a new date if you want to add another one or type \"Done\" to go to the next step")
+    return State.DATE_BIS
+
+def date_bis_response(update : Update, context : CallbackContext) ->State:
+    if update.effective_message.text == 'Done':
+        update.replay_text("You may now want to chose a location for your event, just type it for me...")
+        return State.LOCATION
+    else:
+        text = update.effective_message.text
+        try:
+            date = datetime.datetime.strotime(text, '%d.%m.%Y')
+            # TODO save date
+        except ValueError as error:
+            # handle error
+            pass
+
+        return State.DATE_BIS
+
+def location_response(update: Update, context: CallbackContext) ->State:
+    ...
+
 
 
 def main() -> None:
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
     updater = Updater(TOKEN, use_context=True)
     dispatcher = updater.dispatcher
 
     #Commands handlers
-    dispatcher.add_handler(ConversationHandler(CommandHandler('start', start), {State.SLEEP:}))
-    updater = Updater(TOKEN, use_context=True)
-    dispatcher = updater.dispatcher
-    updater = Updater(TOKEN, use_context=True)
-    dispatcher = updater.dispatcher
+    dispatcher.add_handler(ConversationHandler([CommandHandler('start', start)], {  
+        State.SLEEP : [MessageHandler(Filters.text & (~Filters.command), sleep_response)],
+        State.START : [MessageHandler(Filters.text, start_response)],
+        State.DATE : [MessageHandler(Filters.text, date_response)],
+        State.DATE_BIS : [MessageHandler(Filters.text, date_bis_response)],
+        State.LOCATION : [MessageHandler(Filters.text, location_response)]
+        } , [MessageHandler(Filters.text, error_response)]))
 
-    #Commands handlers
-    dispatcher.add_handler(CommandHandler('start', start))
-    updater = Updater(TOKEN, use_context=True)
-    dispatcher = updater.dispatcher
 
-    #Commands handlers
-    dispatcher.add_handler(CommandHandler('start', start))
-    updater = Updater(TOKEN, use_context=True)
-    dispatcher = updater.dispatcher
-
-    #Commands handlers
-    dispatcher.add_handler(CommandHandler('start', start))
-    
-    #Message handlers
-    dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command), resp))
-
+    #START/STOP
     updater.start_polling()
     updater.idle()
 
